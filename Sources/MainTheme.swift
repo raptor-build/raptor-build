313b11a79e4b6a8ec14920bc60607a2792469b3b
JP Toro <jp@jptoro.dev>
2026-04-03 13:16:27 -0400

    Adopt latest APIs in `MainTheme`.

--- Sources/MainTheme.swift
+++ Sources/MainTheme.swift
@@ -8,20 +8,16 @@
 import Raptor
 
 extension Font {
-    static let advocate = Font(
-        name: "Advocate",
-        sources:
-        FontSource(url: URL(static: "/fonts/Advocate-Regular.woff2"), weight: .regular),
-        FontSource(url: URL(static: "/fonts/Advocate-Medium.woff2"), weight: .medium),
-        FontSource(url: URL(static: "/fonts/Advocate-Bold.woff2"), weight: .bold)
-    )
+    static let advocate = Font(name: "Advocate") {
+        FontSource(file: "Advocate-Regular.woff2", weight: .regular)
+        FontSource(file: "Advocate-Medium.woff2", weight: .medium)
+        FontSource(file: "Advocate-Bold.woff2", weight: .bold)
+    }
 
-    static let mdIO = Font(
-        name: "MDIO",
-        sources:
-        FontSource(url: URL(static: "/fonts/MDIO-Regular.woff2"), weight: .regular),
-        FontSource(url: URL(static: "/fonts/MDIO-Bold.woff2"), weight: .bold)
-    )
+    static let mdIO = Font(name: "MDIO") {
+        FontSource(file: "MDIO-Regular.woff2", weight: .regular)
+        FontSource(file: "MDIO-Bold.woff2", weight: .bold)
+    }
 }
 
 struct MainTheme: Theme {
